0fb717e4507acaa9c81bc689b45aaed8d5019a57
cdfox05 <cdfox2@buffalo.edu>
2025-02-20 15:09:30 -0500

    bfs done??

--- HW3Java/src/ub/cse/algo/Solution.java
+++ HW3Java/src/ub/cse/algo/Solution.java
@@ -10,6 +10,17 @@ public class Solution {
 	private int[] output;
 
 
+	class Edge {
+		int from;
+		int to;
+
+		public Edge(int f, int t)
+		{
+			this.to = t;
+			this.from = f;
+		}
+	}
+
 	public Solution(int node, HashMap<Integer, ArrayList<Integer>> g) {
 
 		startNode = node;
@@ -17,12 +28,25 @@ public class Solution {
 		output = new int[g.size()];
 
 		HashSet<Integer> nodesVisited = new HashSet<>();
-		HashMap<Integer, HashSet<Integer>> edgesVisited = new HashMap<>();
-		HashSet<Integer> edgeSet;
+		HashMap<Integer, List<Edge>> edges = new HashMap<>();
+		HashMap<Integer, HashSet<Edge>> edgesVisited = new HashMap<>();
+		HashSet<Edge> edgeSet;
 		Queue<Integer> todoQueue = new LinkedList<>();
 
+		ArrayList<Integer> eList = new ArrayList<>();
+		for (int i = 0; i < g.size(); i++)
+		{
+			eList = g.get(i);
+			for (int j = 0; j < eList.size(); j++) {
+				if (!edges.containsKey(i)) {
+					edges.put(i, new ArrayList<>());
+				}
+				edges.get(i).add(new Edge(i,eList.get(j)));
+			}
+		}
 
 		nodesVisited.add(node); //adds first node as visited
+
 		todoQueue.add(node);
 
 		for (int i = 0; i < g.size(); i++)
@@ -32,14 +56,26 @@ public class Solution {
 		{
 			node = todoQueue.poll();
 
-			for (int e: g.get(node))
-			{
-				if (!nodesVisited.contains(e)) {
-					todoQueue.add(e);
-					nodesVisited.add(e);
-					output[e] += 1;
-				}
+			if (!edgesVisited.containsKey(node))
+				edgesVisited.put(node, new HashSet<>());
 
+			edgeSet = edgesVisited.get(node);
+
+			for (Edge e: edges.get(node))
+			{
+				if (!edgeSet.contains(e))
+				{
+					int w = e.to;
+					if (!nodesVisited.contains(w))
+					{
+						nodesVisited.add(w);
+						edgeSet.add(e);
+						output[w] = output[e.from] + 1;
+
+						todoQueue.add(w);
+
+					}
+				}
 			}
 		}
 
