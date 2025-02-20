d207ebde00e1d81790e6a191e37a1a9610436bfa
cdfox05 <cdfox2@buffalo.edu>
2025-02-20 14:42:41 -0500

    start of BFS

--- HW3Java/src/ub/cse/algo/Solution.java
+++ HW3Java/src/ub/cse/algo/Solution.java
@@ -1,17 +1,52 @@
 package ub.cse.algo;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+import java.util.*;
 
 public class Solution {
-	  private int startNode;
-	  private HashMap<Integer, ArrayList<Integer>> graph;
-	  public Solution(int node, HashMap<Integer, ArrayList<Integer>> g){
-	    startNode = node;
-	    graph = g;
-	  }
+	private int startNode;
+	private HashMap<Integer, ArrayList<Integer>> graph;
+	private int[] output;
 
-	  public int[] outputDistances(){
-	    return null;
-	  }
+
+	public Solution(int node, HashMap<Integer, ArrayList<Integer>> g) {
+
+		startNode = node;
+		graph = g;
+		output = new int[g.size()];
+
+		HashSet<Integer> nodesVisited = new HashSet<>();
+		HashMap<Integer, HashSet<Integer>> edgesVisited = new HashMap<>();
+		HashSet<Integer> edgeSet;
+		Queue<Integer> todoQueue = new LinkedList<>();
+
+
+		nodesVisited.add(node); //adds first node as visited
+		todoQueue.add(node);
+
+		for (int i = 0; i < g.size(); i++)
+			output[i] = 0;
+
+		while (!todoQueue.isEmpty())
+		{
+			node = todoQueue.poll();
+
+			for (int e: g.get(node))
+			{
+				if (!nodesVisited.contains(e)) {
+					todoQueue.add(e);
+					nodesVisited.add(e);
+					output[e] += 1;
+				}
+
+			}
+		}
+
+
+	}
+
+	public int[] outputDistances() {
+		return output;
+	}
 }
